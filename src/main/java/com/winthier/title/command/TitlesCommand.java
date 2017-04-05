5fa93a0e3f677ce7b0342bfbc31ca7209a4ccff3
StarTux <ths.loy@googlemail.com>
2017-04-05 13:07:21 +0200

    Find players more reliably

--- src/main/java/com/winthier/title/command/TitlesCommand.java
+++ src/main/java/com/winthier/title/command/TitlesCommand.java
@@ -1,7 +1,9 @@
 package com.winthier.title.command;
 
+import com.winthier.playercache.PlayerCache;
 import com.winthier.title.TitlePlugin;
 import java.util.Map;
+import java.util.UUID;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -17,6 +19,15 @@ public class TitlesCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
+    OfflinePlayer findPlayer(String name) {
+        OfflinePlayer result;
+        result = plugin.getServer().getPlayerExact(name);
+        if (result != null) return result;
+        UUID uuid = PlayerCache.uuidForName(name);
+        if (uuid != null) return plugin.getServer().getOfflinePlayer(uuid);
+        return null;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String args[]) {
         try {
@@ -30,8 +41,8 @@ public class TitlesCommand implements CommandExecutor {
                 sender.sendMessage(sb.toString());
             } else if ("List".equalsIgnoreCase(args[0]) && args.length == 2) {
                 String playerName = args[1];
-                OfflinePlayer player = plugin.getServer().getOfflinePlayer(playerName);
-                if (player == null || !player.hasPlayedBefore()) throw new CommandException("Player not found: " + playerName);
+                OfflinePlayer player = findPlayer(playerName);
+                if (player == null) throw new CommandException("Player not found: " + playerName);
                 String name = plugin.database.getPlayerTitle(player);
                 StringBuilder sb = new StringBuilder().append(ChatColor.YELLOW).append("Titles of ").append(playerName).append(":");
                 for (Map.Entry<String, String> entry : plugin.database.listTitles(player).entrySet()) {
@@ -68,8 +79,8 @@ public class TitlesCommand implements CommandExecutor {
             } else if ("Unlock".equalsIgnoreCase(args[0]) && args.length == 3) {
                 String playerName = args[1];
                 String titleName = args[2];
-                OfflinePlayer player = plugin.getServer().getOfflinePlayer(playerName);
-                if (player == null || !player.hasPlayedBefore()) throw new CommandException("Player not found: " + playerName);
+                OfflinePlayer player = findPlayer(playerName);
+                if (player == null) throw new CommandException("Player not found: " + playerName);
                 if (null == plugin.database.getTitle(titleName)) throw new CommandException("Unknown title: " + titleName);
                 plugin.database.unlockTitle(player, titleName);
 
@@ -77,16 +88,16 @@ public class TitlesCommand implements CommandExecutor {
             } else if ("Lock".equalsIgnoreCase(args[0]) && args.length == 3) {
                 String playerName = args[1];
                 String titleName = args[2];
-                OfflinePlayer player = plugin.getServer().getOfflinePlayer(playerName);
-                if (player == null || !player.hasPlayedBefore()) throw new CommandException("Player not found: " + playerName);
+                OfflinePlayer player = findPlayer(playerName);
+                if (player == null) throw new CommandException("Player not found: " + playerName);
                 if (null == plugin.database.getTitle(titleName)) throw new CommandException("Unknown title: " + titleName);
                 if (!plugin.database.lockTitle(player, titleName)) throw new CommandException("Player never had this title.");
                 plugin.send(sender, "&eLocked title %s for player %s (%s).", titleName, playerName, player.getUniqueId());
             } else if ("Set".equalsIgnoreCase(args[0]) && args.length == 3) {
                 String playerName = args[1];
                 String titleName = args[2];
-                OfflinePlayer player = plugin.getServer().getOfflinePlayer(playerName);
-                if (player == null || !player.hasPlayedBefore()) throw new CommandException("Player not found: " + playerName);
+                OfflinePlayer player = findPlayer(playerName);
+                if (player == null) throw new CommandException("Player not found: " + playerName);
                 if (null == plugin.database.getTitle(titleName)) throw new CommandException("Unknown title: " + titleName);
                 if (!plugin.database.playerHasTitle(player, titleName)) throw new CommandException("This title is locked.");
                 plugin.database.setPlayerTitle(player, titleName);
@@ -94,8 +105,8 @@ public class TitlesCommand implements CommandExecutor {
                 if (player.isOnline()) plugin.updatePlayer(player.getPlayer());
             } else if ("Reset".equalsIgnoreCase(args[0]) && args.length == 2) {
                 String playerName = args[1];
-                OfflinePlayer player = plugin.getServer().getOfflinePlayer(playerName);
-                if (player == null || !player.hasPlayedBefore()) throw new CommandException("Player not found: " + playerName);
+                OfflinePlayer player = findPlayer(playerName);
+                if (player == null) throw new CommandException("Player not found: " + playerName);
                 plugin.database.setPlayerTitle(player, null);
                 plugin.send(sender, "Reset title of player %s (%s).", playerName, player.getUniqueId());
                 if (player.isOnline()) plugin.updatePlayer(player.getPlayer());
