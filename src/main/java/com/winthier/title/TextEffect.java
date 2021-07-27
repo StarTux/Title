0f122d1fc9cc2ec9c43a9ff53306a731e1265371
StarTux <ths.loy@googlemail.com>
2021-07-27 14:45:47 +0200

    banner and gradient text effects

--- src/main/java/com/winthier/title/TextEffect.java
+++ src/main/java/com/winthier/title/TextEffect.java
@@ -2,10 +2,11 @@ package com.winthier.title;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -15,58 +16,200 @@ import net.kyori.adventure.text.format.TextFormat;
 public abstract class TextEffect implements TextFormat {
     private static final List<TextEffect> VALUES = new ArrayList<>();
     private static final Map<String, TextEffect> NAME_MAP = new HashMap<>();
+    public static final TextEffect RAINBOW = new TextEffectRainbow();
+    public static final TextEffect SEQUENCE = new TextEffectSequence();
+    public static final TextEffect BANNER = new TextEffectBanner();
+    public static final TextEffect GRADIENT = new TextEffectGradient();
 
-    public static final TextEffect RAINBOW = new TextEffect() {
-            @Getter String name = "rainbow";
+    public static final class TextEffectRainbow extends TextEffect {
+        public static final String NAME = "rainbow";
 
-            @Override public Component format(String in) {
-                final NamedTextColor[] colors = {
-                    NamedTextColor.RED,
-                    NamedTextColor.GOLD,
-                    NamedTextColor.YELLOW,
-                    NamedTextColor.GREEN,
-                    NamedTextColor.DARK_AQUA,
-                    NamedTextColor.BLUE,
-                    NamedTextColor.LIGHT_PURPLE
-                };
-                TextComponent.Builder cb = Component.text();
-                for (int i = 0; i < in.length(); i += 1) {
-                    cb.append(Component.text(in.charAt(i), colors[i % colors.length]));
-                }
-                return cb.build();
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        @Override
+        public TextEffectRainbow with(String... args) {
+            if (args.length != 0) {
+                throw new IllegalArgumentException("args.length != 0: " + Arrays.asList(args));
             }
-        };
+            return this;
+        }
 
-    public static final TextEffect SEQUENCE = new TextEffect() {
-            @Getter String name = "sequence";
-            private List<TextColor> colors;
-
-            /**
-             * @bug This is very much not thread-safe.
-             */
-            @Override public TextEffect with(String... in) {
-                colors = new ArrayList<>(in.length);
-                for (String it : in) {
-                    colors.add(parseTextColor(it));
-                }
-                return this;
+        @Override
+        public Component format(String in) {
+            final NamedTextColor[] colors = {
+                NamedTextColor.RED,
+                NamedTextColor.GOLD,
+                NamedTextColor.YELLOW,
+                NamedTextColor.GREEN,
+                NamedTextColor.DARK_AQUA,
+                NamedTextColor.BLUE,
+                NamedTextColor.LIGHT_PURPLE
+            };
+            TextComponent.Builder cb = Component.text();
+            for (int i = 0; i < in.length(); i += 1) {
+                cb.append(Component.text(in.charAt(i), colors[i % colors.length]));
             }
+            return cb.build();
+        }
+    }
 
-            @Override public Component format(String in) {
-                TextComponent.Builder cb = Component.text();
-                for (int i = 0; i < in.length(); i += 1) {
-                    TextColor color = colors.get(i % colors.size());
-                    cb.append(color != null
-                              ? Component.text(in.charAt(i), color)
-                              : Component.text(in.charAt(i)));
-                }
-                return cb.build();
+    /**
+     * The color sequence colors each letter in another color,
+     * according to the given color sequence, which wraps around if
+     * necessary.
+     */
+    @RequiredArgsConstructor
+    public static final class TextEffectSequence extends TextEffect {
+        public static final String NAME = "sequence";
+        private final List<TextColor> colors;
+
+        public TextEffectSequence() {
+            this(Collections.emptyList());
+        }
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        @Override
+        public TextEffect with(String... args) {
+            List<TextColor> list = new ArrayList<>(args.length);
+            for (String it : args) {
+                list.add(parseTextColor(it));
             }
-        };
+            return new TextEffectSequence(list);
+        }
+
+        @Override
+        public Component format(String in) {
+            if (colors.size() == 0) {
+                throw new IllegalArgumentException("colors.size = 0");
+            }
+            TextComponent.Builder cb = Component.text();
+            for (int i = 0; i < in.length(); i += 1) {
+                TextColor color = colors.get(i % colors.size());
+                cb.append(color != null
+                          ? Component.text(in.charAt(i), color)
+                          : Component.text(in.charAt(i)));
+            }
+            return cb.build();
+        }
+    }
+
+    /**
+     * Spread out the given color sequence over all the letters.
+     */
+    @RequiredArgsConstructor
+    public static final class TextEffectBanner extends TextEffect {
+        public static final String NAME = "banner";
+        private final List<TextColor> colors;
+
+        public TextEffectBanner() {
+            this(Collections.emptyList());
+        }
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        @Override
+        public TextEffect with(String... args) {
+            List<TextColor> list = new ArrayList<>(args.length);
+            for (String it : args) {
+                list.add(parseTextColor(it));
+            }
+            return new TextEffectBanner(list);
+        }
+
+        @Override
+        public Component format(String in) {
+            if (colors.size() == 0) {
+                throw new IllegalArgumentException("colors.size = 0");
+            }
+            TextComponent.Builder cb = Component.text();
+            final int len = in.length();
+            for (int i = 0; i < len; i += 1) {
+                double fraction = (((double) i + 0.5) / (double) len) * (double) colors.size();
+                int index = (int) Math.floor(fraction);
+                TextColor color = colors.get(Math.min(colors.size(), index)); // Just to be sure
+                cb.append(color != null
+                          ? Component.text(in.charAt(i), color)
+                          : Component.text(in.charAt(i)));
+            }
+            return cb.build();
+        }
+    }
+
+    @RequiredArgsConstructor
+    public static final class TextEffectGradient extends TextEffect {
+        public static final String NAME = "gradient";
+        private final List<TextColor> colors;
+
+        protected TextEffectGradient() {
+            this(Collections.emptyList());
+        }
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        @Override
+        public TextEffect with(String... args) {
+            List<TextColor> list = new ArrayList<>(args.length);
+            for (String it : args) {
+                list.add(parseTextColor(it));
+            }
+            return new TextEffectGradient(list);
+        }
+
+        private static int clampRGB(int val) {
+            return Math.min(255, Math.max(0, val));
+        }
+
+        @Override
+        public Component format(String in) {
+            if (colors.size() == 0) {
+                throw new IllegalArgumentException("colors.size = 0");
+            }
+            final int len = in.length();
+            TextComponent.Builder comps = Component.text();
+            for (int i = 0; i < len; i += 1) {
+                String d = "" + in.charAt(i);
+                double percentage = (double) i / (double) (len - 1);
+                double colorPercentage = percentage * (double) (colors.size() - 1);
+                int colorIndexA = (int) Math.floor(colorPercentage);
+                int colorIndexB = (int) Math.ceil(colorPercentage);
+                TextColor colorA = colors.get(colorIndexA);
+                TextColor colorB = colors.get(colorIndexB);
+                double percentageA = (double) colorIndexA / (double) (colors.size() - 1);
+                double percentageB = (double) colorIndexB / (double) (colors.size() - 1);
+                double progressAB = colorIndexA == colorIndexB
+                    ? 0
+                    : (percentage - percentageA) / (percentageB - percentageA);
+                double progressBA = 1.0 - progressAB;
+                double r = (double) colorA.red()   * progressBA + colorB.red()   * progressAB;
+                double g = (double) colorA.green() * progressBA + colorB.green() * progressAB;
+                double b = (double) colorA.blue()  * progressBA + colorB.blue()  * progressAB;
+                TextColor color = TextColor.color(clampRGB((int) Math.round(r)),
+                                                  clampRGB((int) Math.round(g)),
+                                                  clampRGB((int) Math.round(b)));
+                comps.append(Component.text(d, color));
+            }
+            return comps.build();
+        }
+    };
 
     static {
         VALUES.add(RAINBOW);
         VALUES.add(SEQUENCE);
+        VALUES.add(GRADIENT);
+        VALUES.add(BANNER);
         for (TextEffect it : VALUES) {
             NAME_MAP.put(it.getName(), it);
         }
